76c2e1c3101e5de78f2e6731929c08be2744a0b9
Vitor <vitortheof@gmail.com>
2026-07-02 19:19:50 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import entities.Department;
 import entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -16,6 +17,25 @@ public class Program {
         System.out.println("=== TEST 1: seller findById ===:");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("=== TEST 2: seller findByDepartment ===:");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller s : list) {
+            System.out.println(s);
+        }
+
+        System.out.println("=== TEST 3: seller findAll ===:");
+        list = sellerDao.findAll();
+        for (Seller s : list) {
+            System.out.println(s);
+        }
+
+        System.out.println("=== TEST 3: seller update ===:");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update complete");
 
     }
 }
